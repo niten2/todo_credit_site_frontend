69cdaf7e7c34581fca96fb9b9e1a5ad65ce18350
Niten <niten2@yandex.ru>
2018-02-06 20:19:49 +0700

    commit - Вт фев  6 20:19:49 +07 2018

--- src/config/notification.ts
+++ src/config/notification.ts
@@ -1,63 +1,44 @@
 // import Notification from 'src/config/notification'
 // Notification.success("")
+// Notification.error("")
 // Notification.info("")
 // Notification.warning("")
-// Notification.errors("")
 
 import UIStore from "src/store"
 
 export default {
 
-	success(message: string) {
-		UIStore.notificationSystem.addNotification({
+  success(message: string) {
+    UIStore.notificationSystem.addNotification({
       message,
-			position: "br",
+      position: "br",
       level: 'success'
     })
-	},
+  },
 
-	info(message: string) {
-		UIStore.notificationSystem.addNotification({
+  info(message: string) {
+    UIStore.notificationSystem.addNotification({
       message,
-			position: "br",
+      position: "br",
       level: 'info'
     })
-	},
+  },
 
-	warning(message: string) {
-		UIStore.notificationSystem.addNotification({
+  warning(message: string) {
+    UIStore.notificationSystem.addNotification({
       message,
-			position: "br",
+      position: "br",
       level: 'warning',
     });
-	},
+  },
 
-	error(message: string) {
-		UIStore.notificationSystem.addNotification({
+  error(message: string) {
+    UIStore.notificationSystem.addNotification({
       title: 'An error occured',
       message,
-			position: "br",
+      position: "br",
       level: 'error'
     })
-	},
-
-	// errors(errors = []) {
-	// 	if (errors.length === 1) {
-	// 		this.error(errors[0]);
-	// 		return
-	// 	}
-
-	// 	errors = errors.reduce((message, error) => {
-	// 		return message + '<li>'+error+'</li>';
-	// 	}, '');
-
-    // let message = '<ul>'+errors+'</ul>';
-
-	// 	UIStore.notificationSystem.addNotification({
-      // title: 'Following errors occured',
-      // message,
-      // level: 'error'
-    // });
-	// }
+  },
 
 }
