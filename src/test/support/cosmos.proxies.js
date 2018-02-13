8786d2686b57b89842c3c7ed4475deeae256da0f
Niten <niten2@yandex.ru>
2018-02-13 19:35:28 +0700

    fix proxies

---
+++ src/test/support/cosmos.proxies.js
@@ -0,0 +1,43 @@
+// import createRouterProxy from 'react-cosmos-router-proxy'
+// import createLocalStorageProxy from 'react-cosmos-localstorage-proxy'
+// import createApolloProxy from './react_cosmos_apollo_proxy'
+
+// import 'public/css/style.css'
+// import { typeDefs, mocks }  from 'src/test/support/graphql'
+
+// export default [
+//   createApolloProxy({
+//     typeDefs,
+//     mocks,
+//   }),
+//   createRouterProxy(),
+//   createLocalStorageProxy(),
+// ]
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _reactCosmosRouterProxy = require('react-cosmos-router-proxy');
+
+var _reactCosmosRouterProxy2 = _interopRequireDefault(_reactCosmosRouterProxy);
+
+var _reactCosmosLocalstorageProxy = require('react-cosmos-localstorage-proxy');
+
+var _reactCosmosLocalstorageProxy2 = _interopRequireDefault(_reactCosmosLocalstorageProxy);
+
+var _react_cosmos_apollo_proxy = require('./react_cosmos_apollo_proxy');
+
+var _react_cosmos_apollo_proxy2 = _interopRequireDefault(_react_cosmos_apollo_proxy);
+
+require('public/css/style.css');
+
+var _graphql = require('src/test/support/graphql');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = [(0, _react_cosmos_apollo_proxy2.default)({
+  typeDefs: _graphql.typeDefs,
+  mocks: _graphql.mocks
+}), (0, _reactCosmosRouterProxy2.default)(), (0, _reactCosmosLocalstorageProxy2.default)()];
