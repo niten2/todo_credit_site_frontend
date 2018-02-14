c1172521c46debc7e605309c5de5afcb5d16e5a5
Niten <niten2@yandex.ru>
2018-02-14 17:14:04 +0700

    commit - Ср фев 14 17:14:04 +07 2018

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   publicPath: "public",
   proxiesPath: "./src/test/support/cosmos.proxies",
-
-  // webpackConfigPath: "./src/test/support/webpack.config",
-
   webpackConfigPath: 'react-scripts-ts/config/webpack.config.dev',
 }
